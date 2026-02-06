2da0241864cf435b9bcc55422e24968ace71179c
Geovan Borges <geovansb@gmail.com>
2026-02-06 13:24:20 -0300

    enable s3 bucket encryption

--- infra/terraform/s3.tf
+++ infra/terraform/s3.tf
@@ -22,3 +22,12 @@ resource "aws_s3_bucket_public_access_block" "site" {
     ignore_public_acls      = true
     restrict_public_buckets = true
 }
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "site" {
+    bucket = aws_s3_bucket.site.id
+    rule {
+        apply_server_side_encryption_by_default {
+            sse_algorithm = "AES256"
+        }
+    }
+}
